5cc0d3517c39e176219dcee41b1f048f231a3b60
wlt30 <wlt@ualberta.ca>
2018-11-01 10:50:25 -0600

    added clear functionality fr all 3 main OS

--- miniproject1.py
+++ miniproject1.py
@@ -4,32 +4,35 @@ import sqlite3
 import getpass
 import os
 import datetime
+import platform
 
-def exitApp():
-        os.system("cls")
+def exitApp(database):
+        os.system(cls)
         print('EXITING\nThank you for using this Ride Finder')
         time.sleep(2)
+        database.commit()
+        database.close()
         sys.exit()
 
-def entry():
+def entry(database):
     #controlled login screen, only allows for Login and Register options
     login_option = 0
     while(login_option<1 or login_option>2):
         login_option = (input("Select 1 to login as an existing user, or press 2 to register\nAt any point, type EXIT to end your session\n"))
         if login_option.upper() == "EXIT":
-            exitApp()
+            exitApp(database)
         if not (login_option[0].isdigit()):
             print("Invalid entry, please try again")
             time.sleep(1)
             login_option = 0
-            os.system('cls')
+            os.system(cls)
         else:
             login_option = int(login_option)
-            os.system('cls')
+            os.system(cls)
     return login_option
 
-def login(dbcursor):
-    os.system('cls')
+def login(database, dbcursor):
+    os.system(cls)
     validEmail = False
     validPass = False
     print("LOGIN\nAt any point, type BACK to go back")
@@ -37,9 +40,9 @@ def login(dbcursor):
         # receives user input for email and checks the validity of this email address in the database
         email = input("Email address: ")
         if(email.upper() == "EXIT"):
-            exitApp()
+            exitApp(database)
         if(email.upper() == "BACK"):
-            entry()
+            entry(database)
         dbcursor.execute("SELECT email FROM members WHERE email = \""+email +"\"")
         emailList = dbcursor.fetchall()
         if len(emailList)>0 and email != "email":
@@ -66,7 +69,7 @@ def login(dbcursor):
     time.sleep(1)
 
 def register(dbcursor):
-    os.system('cls')
+    os.system(cls)
     print("REGISTER\nPlease provide the following:")
     availableEmail = False
     validName = False
@@ -140,7 +143,7 @@ def register(dbcursor):
     return email
 
 def offerRide(dbcursor, member):
-    os.system('cls')
+    os.system(cls)
     print("Offer a Ride")
     driver = str(member)
     dbcursor.execute("SELECT MAX(rno) FROM rides")
@@ -342,6 +345,7 @@ def offerRide(dbcursor, member):
                 continue
         elif cno == "":
             validCar= True
+            # only way to get out of loop
             cno = "NULL"
         else:
             print("Something went wrong, please try again")
@@ -350,15 +354,15 @@ def offerRide(dbcursor, member):
     dbcursor.execute("INSERT INTO rides VALUES (\""+rno+"\", \""+pricePerSeat+"\", \""+date+"\", \""+noSeats+"\", \""+lugDesc+"\", \""+str(src)+"\", \""+str(dst)+"\", \""+driver+"\", \""+cno+"\")")
     for item in enroutes:
         dbcursor.execute("INSERT INTO enroute VALUES (\"" +rno+"\", \""+item[0]+"\")")
-    os.system('cls')
+    os.system(cls)
                 
 def searchForRide(dbcursor):
-    os.system('cls')
+    os.system(cls)
     print("Search for a Ride")
 
 #Working on bookings
 def Bookings(dbcursor,member): #Assume member passed it the user email logged on
-    os.system('cls')
+    os.system(cls)
     Finished = False
     while not Finished:
         user_input= input("What would you like to do?\n1.List Current Bookings You Offer\n2.List Other Members Bookings\n 3.Book Rides\n4.Cancel Bookings\n5.Finished")
@@ -377,16 +381,16 @@ def Bookings(dbcursor,member): #Assume member passed it the user email logged on
 
 
 def postRideRequest(dbcursor):
-    os.system('cls')
+    os.system(cls)
     print("Post Ride Request")
 
 
 def searchAndDeleteRequest(dbcursor):
-    os.system('cls')
+    os.system(cls)
     print("Search and Delete Ride Request")
 
 def mainMenu(database, dbcursor, member):
-    os.system('cls')
+    os.system(cls)
     exiting = False
     while not exiting:
         user_option = input("What would you like to do?\n1.Offer a Ride\n2.Search for Rides\n3.Book Members or Cancel Bookings\n4.Post Ride Request\n5.Search and Delete Ride Request\n6.Logout\nAt any point, type EXIT to end your session\n")
@@ -401,7 +405,6 @@ def mainMenu(database, dbcursor, member):
 
             time.sleep(0.5)
             Bookings(dbcursor,member)
-
         elif(user_option == "4"):
             time.sleep(0.5)
             postRideRequest(dbcursor)
@@ -410,14 +413,24 @@ def mainMenu(database, dbcursor, member):
             searchAndDeleteRequest(dbcursor)
         elif(user_option == "6"):
             print("Logging out...")
+            database.commit()
+            database.close()
             time.sleep(1)
-            os.system('cls')
+            os.system(cls)
             main()
         elif(user_option.upper() == "EXIT"):
-            exitApp()
+            exitApp(database)
+            # only way to properly exit the program
         else:
             print("Not a valid option, please try again")
 
+if((platform.system()) == "Windows"):
+    cls = 'cls'
+elif((platform.system()) == "Linux"):
+    cls = 'clear'
+elif((platform.system()) == "Darwin"):
+    cls = 'clear'
+        
 def main():
     exiting = False
     validDatabase = False
@@ -436,10 +449,17 @@ def main():
                     continue
             else:
                 validDatabase = True
+
+    if((platform.system()) == "Windows"):
+        cls = 'cls'
+    elif((platform.system()) == "Linux"):
+        cls = 'clear'
+    elif((platform.system()) == "Darwin"):
+        cls = 'clear'
             
     print("Welcome to Ride Finder")
     time.sleep(0.5)
-    login_option = entry()
+    login_option = entry(database)
     if(login_option == 1):
         member = login(dbcursor)
     else:
