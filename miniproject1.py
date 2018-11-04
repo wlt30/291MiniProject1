f0b15fae900285baf24326f9381d59bf897af8e7
DeveloperJeffb <33044463+DeveloperJeffb@users.noreply.github.com>
2018-11-04 15:15:55 -0700

    adding bokings function & post ride requests

--- miniproject1.py
+++ miniproject1.py
@@ -8,6 +8,8 @@ import platform
 import searchRides
 import searchRequest
 
+from datetime import datetime
+
 def exitApp(database):
         os.system(cls)
         print('EXITING...\nThank you for using Ride Finder')
@@ -185,7 +187,7 @@ def offerRide(dbcursor, member):
                     print("Invalid date format, please try again (e.g. YYYY-MM-DD)")
                     continue
             validDate = True
-                                                                                                
+
     while not validNoSeats:
         noSeats = input("Enter the number of seats: ")
         if noSeats.isdigit():
@@ -193,7 +195,7 @@ def offerRide(dbcursor, member):
         else:
             print("Invalid input format, please try again ")
             continue
-                                                                                                
+
     while not validPricePerSeat:
         pricePerSeat = input("Enter a price per seat: ")
         if pricePerSeat.isdigit():
@@ -201,21 +203,21 @@ def offerRide(dbcursor, member):
         else:
             print("Invalid input format, please try again ")
             continue
-        
-                                                                                                
+
+
     while not validLugDesc:
         lugDesc = input("Enter a luggage description (max 10 characters): ")
         if len(lugDesc) >10 or len(lugDesc)==0:
             print("Invalid input format, please try again ")
             continue
         validLugDesc = True
-                                                                                                
+
     while not validSrc:
         entry = input("Enter a source location (max 16 characters): ")
         if len(entry) >16 or len(entry)==0:
             print("Invalid input format, please try again ")
             continue
-        
+
         dbcursor.execute("SELECT * FROM locations WHERE lcode LIKE \"%"+entry+"%\"  OR city LIKE \"%"+entry+"%\" OR prov LIKE \"%"+entry+"%\" OR address LIKE \"%"+entry+"%\"")
         srcOptions = dbcursor.fetchall()
         x = 0
@@ -240,7 +242,7 @@ def offerRide(dbcursor, member):
                     else:
                         print("Invalid choice, please try again")
                         x = 0
-                        continue    
+                        continue
         elif(len(srcOptions)==1):
             src = srcOptions[0][0]
             print(srcOptions)
@@ -248,7 +250,7 @@ def offerRide(dbcursor, member):
         else:
             print("Sorry, we couldn't find any lcode, city, prov or address with that tag")
             continue
-    
+
     while not validDst:
         entry = input("Enter a destination location (max 16 characters): ")
         if len(entry) >16 or len(entry)==0:
@@ -278,7 +280,7 @@ def offerRide(dbcursor, member):
                     else:
                         print("Invalid choice, please try again")
                         x = 0
-                        continue    
+                        continue
         elif(len(dstOptions)==1):
             dst = dstOptions[0][0]
             print(dstOptions)
@@ -298,7 +300,7 @@ def offerRide(dbcursor, member):
             validEnroutes = True
             ## only way to pass this step
             continue
-        
+
         dbcursor.execute("SELECT * FROM locations  WHERE lcode LIKE \"%"+stop+"%\"  OR city LIKE \"%"+stop+"%\" OR prov LIKE \"%"+stop+"%\" OR address LIKE \"%"+stop+"%\"")
         stopOptions = dbcursor.fetchall()
         x = 0
@@ -323,13 +325,13 @@ def offerRide(dbcursor, member):
                     else:
                         print("Invalid choice, please try again")
                         x = 0
-                        continue    
+                        continue
         elif(len(stopOptions)==1):
             enroutes.append(stopOptions[0])
             print(stopOptions)
         else:
             print("Sorry, we couldn't find any lcode, city, prov or address with that tag")
-        
+
 
     while not validCar:
         cno = input("Please enter a car number or press ENTER to skip: ")
@@ -358,7 +360,7 @@ def offerRide(dbcursor, member):
     for item in enroutes:
         dbcursor.execute("INSERT INTO enroute VALUES (\"" +rno+"\", \""+item[0]+"\")")
     os.system(cls)
-                
+
 def searchForRide(dbcursor):
     os.system(cls)
     print("Search for a Ride")
@@ -368,24 +370,262 @@ def Bookings(dbcursor,member): #Assume member passed it the user email logged on
     os.system(cls)
     Finished = False
     while not Finished:
-        user_input= input("What would you like to do?\n1.List Current Bookings You Offer\n2.List Other Members Bookings\n 3.Book Rides\n4.Cancel Bookings\n5.Finished")
+        user_input= input("What would you like to do?\n1.List all bookings on ride user offers\n2.Cancel Bookings on rides user offer\n3.List All rides user offers and number of availble seats for each ride \n4.Book Rides\n5.Finished\n")
+
         if(user_input == "1"):
-            print("Listing current booking you offer\n");
-            dbcursor.execute("SELECT * FROM rides where rides.driver = ?", member)
-            #Have a for loop that knows length of current bookings & prints booking 5 at a time
-            #TEST option 1 & Correct
+            print("-------------List All rides member offers and number of availble seats for each ride -------------\n");
+            print("member is: ", member, "\n")
+            dbcursor.execute("SELECT b.bno, r.rno, r.driver, b.email FROM bookings b, rides r WHERE r.rno = b.rno and r.driver = ?\n", (member,)) #multiple rno, but with differrnt bno tags
+            Booking = dbcursor.fetchall() #all results will be stored into the list of tuples in Bookings
+
+            i = 0;
+            for row in Booking:
+                print("|bno: %s|rno: %s|driver: %s|passenger: %s|\n"%(str(row[0]), str(row[1]), str(row[2]), str(row[3])))
+                i = i+1;
+                if i%5 ==0:
+                    more = input("Print more Bookings offered (yes/no)\n")
+                    if more.lower() == "no":
+                        break
+                    else:
+                        continue
+            print("-------------Finished Listing Bookings-------------")
             #Proceed with listing other  member rides afterwards, except current member
-        elif(use_input == "5"):
+        elif (user_input =="2"):
+            print("-------------Cancel Bookings on Rides user Offers-------------")
+
+            #-------------------------------Test-------------------------------
+            # dbcursor.execute("SELECT r.rno, b.bno, r.driver FROM bookings b, rides r where r.rno =b.rno and r.driver =?;", (member,))
+            # test= dbcursor.fetchall()
+            # for each in test:
+            #     print(each)
+            #------------------------------------------------------------------
+
+            check = False
+            while check == False:
+                bno = int(input("Enter bno: ")) #
+                dbcursor.execute("SELECT b.bno FROM bookings b, rides r where r.rno =b.rno and b.bno = ? and r.driver =?;",(bno,member,))
+                if dbcursor.fetchone() == None:
+                    print("Bno Does not exist Try Again: ")
+                else:
+                    print("Bno Entered is: rno: ",  bno)
+                    dbcursor.execute("select b.email, b.rno from bookings b where b.bno =?", (bno,))
+                    data = dbcursor.fetchone()
+                    email = data[0]
+                    rno = data[1]
+                    print("Email: ", email, "Rno: ", rno)
+
+
+                    print(email)
+
+                    dbcursor.execute("Delete from bookings where bno = ?", (bno,))
+
+                    dbcursor.execute("INSERT into inbox values(?,?,?,?,?,?);",(email, datetime.now().strftime('%Y-%m-%d %H:%M:%S'),member,"Your booking Has been cancelled",rno,'n',))
+                    dbcursor.execute("Select * from inbox")
+                    ya =dbcursor.fetchall()
+                    for each in ya:
+                        print(each)
+
+                    check =True
+
+
+
+
+        elif(user_input == "3"):
+            print("-------------List All rides member offers and number of availble seats for each ride -------------\n");
+            print("member is: ", member, "\n")
+            #Your system should list all rides the member offers with the number of available seats for each ride (i.e., seats that are not booked).
+            #Include rides that have bookings and rides that do not have bookings  [r.rno = b.rno]
+
+            #dbcursor.execute(" SELECT b.bno,r.rno,r.driver, b.email, r.seats, b.seats FROM bookings b, rides r WHERE r.driver = ? and r.rno<>b.rno UNION SELECT b.bno,r.rno,r.driver, b.email, r.seats, b.seats FROM bookings b, rides r WHERE r.driver =? and r.rno=b.rno", (member,member,))
+            print("SWAG")
+            dbcursor.execute("SELECT b.bno, r.rno, r.driver, b.email, r.seats, b.seats FROM bookings b, rides r where r.rno = b.rno and r.driver = ?\n", (member,)) #multiple rno, but with differrnt bno tags
+            Booking = dbcursor.fetchall() #all results will be stored into the list of tuples in Bookings
+
+            i = 0;
+            for row in Booking:
+                print("|bno: %s|rno: %s|driver: %s|passenger: %s|Ride Seats: %s|Seats Booked: %s|Seats available: %s|\n"%(str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]), str(int(row[4] - int(row[5]))) ))
+                i = i+1;
+                if i%5 ==0:
+                    more = input("Print more Rides offered (yes/no)\n")
+                    if more.lower() == "no":
+                        break
+                    else:
+                        continue
+            print("-------------Finished Listing Bookings-------------")
+
+
+
+        elif(user_input =="4"):       #selects rno from all rides of ride table, As long as its inputed correctly it should be fine
+            print("-------------Booking Rides-------------")
+            check1 = False
+            while check1 == False:
+                rno = int(input("Enter Rno: ")) #
+                dbcursor.execute("SELECT rno FROM rides where rno = ?;",(rno,))
+                if dbcursor.fetchone() == None:
+                    print("Rno Does not exist Try Again: ")
+                else:
+                    print("Rno Entered is: rno: ",  rno)
+                    check1 =True
+
+            check2 = False
+            while check2 ==False:
+                email =input("Enter Email: ")
+                dbcursor.execute("Select email FROM Members where email =?;",(email,))
+                if dbcursor.fetchone() == None:  #check if none first if not none check if the email match rno
+                    print("Email does not exist Try Again: ")
+                else:
+                    print("Emial Entered is: ", email)
+                    check2 = True
+
+            check3 = False
+            while check3 ==False:
+                pickup = input("Enter pickup Location code: ")
+                dbcursor.execute("Select lcode from locations where lcode = ?;", (pickup,))
+                if dbcursor.fetchone() == None:
+                    print("lcode for pick up does not exist Try Again")
+                else:
+                    print("lcode entered is: ", pickup)
+                    check3 = True
+
+            check4 = False
+            while check4 ==False:
+                dropoff = input("Enter Drop off location: ")
+                dbcursor.execute("Select lcode from locations where lcode = ?;", (dropoff,))
+                if dbcursor.fetchone() == None:
+                    print('lcode for drop off does not exist Try Again')
+                else:
+                    print("lcode for dropoff entered is: ", dropoff)
+                    check4 = True
+
+            check5 = False
+            while check5 == False:
+                cost = int(input("Enter Cost per seat: "))      #accepts integer
+                dbcursor.execute("Select price from rides where rno =?;", (rno,))
+                if cost != dbcursor.fetchone()[0]:
+                    print("Cost for ride is not correct please Try Again")
+                else:
+                    print("Cost entered per seat: ", cost)
+                    check5 = True
+
+            check6 = False
+            dbcursor.execute("Select seats from rides where rno = ?;", (rno,))
+            seat_check = dbcursor.fetchone()[0]
+            dbcursor.execute("Select seats from bookings where rno =? ", (rno,))
+            already_book = dbcursor.fetchone()
+            if already_book ==None:
+                already_book = 0
+            else:
+                already_Booked = already_booked[0]     #Changed
+            print("Seats Entere:", seat_check)
+            print("already Booked: ", already_Booked)
+            seat_check = seat_check - already_Booked
+            print("Seats Available: ", seat_check)
+            dbcursor.execute("Select bno from bookings")
+            bno =  dbcursor.fetchall()[-1][0] +1 #Unique bno = last bno +1
+            print("Last bno is: ", (bno-1), "New bno is: ", bno)
+
+
+            while check6 == False:
+                seatsbook = int(input("Enter amount of seats you want to book: "))
+                if  (seat_check - seatsbook) < 0:
+                    choice = input("The number of seats booked exceeds the number of seats offered\nDo you Want to continue to book the ride? (yes/no) ")
+                    if choice.lower() == "yes":
+                        print("You have confirmed your bookings")
+                        # CREATE SQL to update to the table
+                        dbcursor.execute("INSERT into Bookings values(?,?,?,?,?,?,?);",(bno,email,rno,cost,seatsbook,pickup,dropoff))
+                        print("A msg has sent to member who's ride has been booked")
+
+                        # dbcursor.execute("Select * from Bookings;")
+                        # inbox = dbcursor.fetchall()
+                        # for each in inbox:
+                        #     print(each)
+
+                        #Test Inbox Sent-------------------
+                        # dbcursor.execute("Select * from inbox;")
+                        # inbox = dbcursor.fetchall()
+                        # for each in inbox:
+                        #     print(each)
+                        #
+
+                        dbcursor.execute("INSERT into inbox values(?,?,?,?,?,?);",(email, datetime.now().strftime('%Y-%m-%d %H:%M:%S'),member,"New Booking",rno,'n',))
+
+                        # dbcursor.execute("Select * from inbox;")
+                        # inbox2 = dbcursor.fetchall()
+                        # for each in inbox2:
+                        #     print(each)
+                        #Test Inbox Sent------------------
+
+                        check6 =True
+                    elif choice.lower() == "no":
+                        print("No ride has been booked\nThank you")
+                        check6 = True
+                else:
+                    dbcursor.execute("INSERT into Bookings values(?,?,?,?,?,?,?)",(bno,email,rno,cost,seatsbook,pickup,dropoff))
+
+                    print("Your ride has been booked!\n A msg has been sent to the member who's ride has been booked  ")
+                    check6 =True
+
+            #how can i take take in mulltiple inputs at the same time
+            #Check if seats going to be book > seats offered
+
+
+
+            # The member should be able to select a ride and book a member for that ride by entering the member email, the number of seats booked, the cost per seat, and pickup and drop off location codes. Your system should assign a unique booking number (bno) to the booking. Your system should give a warning if a ride is being overbooked (i.e. the number of seats booked exceeds the number of seats offered), but will allow overbooking if the member confirms it. After a successful booking, a proper message should be sent to the other member that s/he is booked on the ride.
+
+        elif(user_input == "5"):
             print("Returning to the Main Menu\n")
             Finished = True
 
-        else:
-            print("Not a valid option please try again\n")
 
 
-def postRideRequest(dbcursor):
+def postRideRequest(dbcursor,member):
     os.system(cls)
     print("Post Ride Request")
+    rdate = input("Enter Return Date (YYYY-MM-DD): ")
+
+    check1 = False
+    while check1 ==False:
+        pickup = input("Enter pickup Location code: ")
+        dbcursor.execute("Select lcode from locations where lcode = ?;", (pickup,))
+        if dbcursor.fetchone() == None:
+            print("lcode for pick up does not exist Try Again")
+        else:
+            print("lcode entered is: ", pickup)
+            check1 = True
+
+    check2 = False
+    while check2 ==False:
+        dropoff = input("Enter dropoff Location code: ")
+        dbcursor.execute("Select lcode from locations where lcode = ?;", (dropoff,))
+        if dbcursor.fetchone() == None:
+            print("lcode for dropoff does not exist Try Again")
+        else:
+            print("lcode entered is: ", dropoff)
+            check2 = True
+
+
+
+    amount = input("Enter amount willing to pay per seat: ")
+#-----------------------TEST----------------------
+    dbcursor.execute("Select * from requests")
+    requests = dbcursor.fetchall()
+    print("Requsets Before members adds")
+    for each in requests:
+        print(each)
+#-------------------------------------------------
+    dbcursor.execute("Select rid from requests")
+    rid =  dbcursor.fetchall()[-1][0] +1 #Unique bno = last bno +1
+
+    #requests(rid, email, rdate, pickup, dropoff, amount)
+    dbcursor.execute("INSERT into requests VALUES(?,?,?,?,?,?)", (rid,member, rdate, pickup, dropoff, amount,))
+
+#-----------------------TEST----------------------
+    dbcursor.execute("Select * from requests")
+    requests2 = dbcursor.fetchall()
+    print("\nRequsets Before members adds")
+    for each in requests2:
+        print(each)
+#-------------------------------------------------
 
 
 def searchAndDeleteRequest(dbcursor):
@@ -410,7 +650,7 @@ def mainMenu(database, dbcursor, member):
             Bookings(dbcursor,member)
         elif(user_option == "4"):
             time.sleep(0.5)
-            postRideRequest(dbcursor)
+            postRideRequest(dbcursor, member)
         elif(user_option == "5"):
             time.sleep(0.5)
             searchRequest.searchAndDeleteRequest(database, dbcursor, member)
@@ -444,7 +684,7 @@ elif((platform.system()) == "Linux"):
     cls = 'clear'
 elif((platform.system()) == "Darwin"):
     cls = 'clear'
-        
+
 def main():
     exiting = False
     validDatabase = False
@@ -463,7 +703,7 @@ def main():
                 continue
         else:
             validDatabase = True
-            
+
     print("Welcome to Ride Finder")
     time.sleep(0.5)
     login_option = entry(database)
@@ -471,7 +711,7 @@ def main():
         member = login(database, dbcursor)
     else:
         member = register(dbcursor)
-        
+
     while not exiting:
         printInbox(database, dbcursor, member)
         mainMenu(database, dbcursor, member)
